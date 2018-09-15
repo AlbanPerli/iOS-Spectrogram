329d2a726bfb87e4e25b89b2fdb719e98a408bba
Alban Perli <alban.perli@gmail.com>
2018-09-15 09:02:23 +0200

    Removed func from playground

--- Playground.playground/Pages/Audio.xcplaygroundpage/Contents.swift
+++ Playground.playground/Pages/Audio.xcplaygroundpage/Contents.swift
@@ -4,42 +4,19 @@ import UIKit
 import UIImage_Spectrogram
 
 let acqCtx = AcquisitionContext(fftSize: 1024, sampleRate: 8000)
-
 acqCtx.frequencyStepForIndex(1)
-
-
-func loadArrayOf<T:FloatingPoint>(_ type:T.Type,from fileName:String, ofType:String, fromBundle:Bool = false) -> [T]? {
-    var filePath:URL? = nil
-    if fromBundle {
-        filePath = DataLoader.documentsDirectory().appendingPathComponent(fileName)
-    }else{
-        filePath = DataLoader.urlForBundleFile(named: fileName, ofType: ofType)
-    }
-    
-    if let path = filePath {
-        if let str = try? String(contentsOf: path, encoding: String.Encoding.utf8){
-            let comp = str.components(separatedBy: ";")
-            
-            print(type)
-            
-            switch type {
-            case is Double.Type:
-                return comp.map{ Double($0) } as? [T]
-            case is Float.Type:
-                return comp.map{ Float($0) } as? [T]
-            default: print("NoType"); break
-            }
-            
-        }
-    }
-    return nil
-}
 
 if let filePath = Bundle.main.url(forResource: "toujours", withExtension: "wav") {
     
+    if let rawAudio = DataLoader.loadAudioSamplesArrayOf(Float.self, atUrl: filePath){
+        rawAudio.map{ $0 }
+        
+        let audioChunks = rawAudio.chunks(acqCtx.fftSize)
+        
+        
+    }
     
-    let rawAudio = loadArrayOf(Float.self, from: "toujours", ofType: "wav", fromBundle:true)
-    
+}
     /*
     let infos = acqCtx.durationInfosForSamples(rawAudio!)
     
@@ -58,6 +35,5 @@ if let filePath = Bundle.main.url(forResource: "toujours", withExtension: "wav")
     let magnitudes = spectrogramValuesForSignal(input: rawAudio!, chunkSize: 512)
     let image2 = ImageUtils.drawMagnitudes(magnitudes: magnitudes, width: 640, height: 480)
      */
-    
-}
+
 
